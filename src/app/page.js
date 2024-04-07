d2af4963fe889495de6341aa2210f196f3a836b6
dwang88 <davidwang1089@gmail.com>
2024-04-07 16:06:36 -0700

    style changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useRef } from 'react';
+import logo from "./logo.png";
 
 export default function VideoTranscriptionPage() {
   const [transcriptLines, setTranscriptLines] = useState([]);
@@ -74,51 +75,63 @@ export default function VideoTranscriptionPage() {
     a.click();
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
+    console.log(logo);
   };
 
   return (
     <>
       <div>
-        <h1 style={{textAlign: "center"}}>Real Time Audio Input To Text</h1>
+      <h1 style={{textAlign: "center"}}>
+        Edu Optimize
+      <img
+        style={{ height: "55px", verticalAlign: "middle", marginLeft: "10px", marginTop: "0px"}}
+        src={'https://media.discordapp.net/attachments/717217667500736534/1226666717913284628/logooo-removebg-preview.png?ex=66259967&is=66132467&hm=62491c3445b9789894fcf7a11482fecc069b8595c8cbe7252dfa3d07f6ff9cd6&=&format=webp&quality=lossless&width=1000&height=1000'}
+        alt="Logo"
+      />
+      </h1>
       </div>
-      <div>
-        <button onClick={handleMicrophoneInput}>Record Audio</button>
-        <button onClick={stopRecording} style={{marginLeft: "20px"}}>Pause Recording</button>
-        <button onClick={clearTranscript} style={{marginLeft: "20px"}}>Clear Transcript</button>
-        <button onClick={saveTranscript} style={{marginLeft: "20px"}}>Save Transcript</button>
-        <h2>Transcript:</h2>
-        <div ref={transcriptRef} style={{backgroundColor: "white", color: "black", padding: "20px"}}>
-          {transcriptLines.map((line, index) => (
-            <div key={index} className={line.timestamp === selectedTimestamp ? "highlighted-line" : ""}>
-              <span>{line.timestamp}: </span>
-              <span>{line.text}</span>
-            </div>
-          ))}
-        {transcriptWords.length === 0 && (
-            <p>Start recording for transcript.</p>
-          )}
-        </div>
-        <h2>Search Transcript:</h2>
-        <div>
-          <input type="text" value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
-          <button onClick={handleSearch}>Search</button>
-        </div>
-        <div>
-          {foundWords.length > 0 && (
-            <div>
-              <h3>Found Words:</h3>
-              <div style={{backgroundColor: "white", color: "black", padding: "20px"}}>
-              {foundWords.map((foundWord, index) => (
-                <p className="search" key={index} onClick={() => scrollToLine(foundWord.timestamp)} style={{cursor:'pointer'}}>
-                  {foundWord.timestamp}: {foundWord.word}
-                </p>
-              ))}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ flex: '1', marginRight: '20px', marginTop: "30px" }}>
+          <button onClick={handleMicrophoneInput}>Record Audio</button>
+          <button onClick={stopRecording} style={{marginLeft: "20px"}}>Pause Recording</button>
+          <button onClick={clearTranscript} style={{marginLeft: "20px"}}>Clear Transcript</button>
+          <h2>Transcript:</h2>
+          <div ref={transcriptRef} style={{backgroundColor: "white", color: "black", padding: "20px"}}>
+            {transcriptLines.map((line, index) => (
+              <div key={index} className={line.timestamp === selectedTimestamp ? "highlighted-line" : ""}>
+                <span>{line.timestamp}: </span>
+                <span>{line.text}</span>
               </div>
-            </div>
-          )}
-          {foundWords.length === 0 && (
-            <p style={{backgroundColor: "white", color: "black", padding: "20px"}}>No matching words found.</p>
-          )}
+            ))}
+          {transcriptWords.length === 0 && (
+              <p>Start recording for transcript.</p>
+            )}
+          </div>
+          <button onClick={saveTranscript} style={{marginTop: "20px"}}>Save Transcript</button>
+        </div>
+        <div style={{ flex: '1', marginTop: "75px"}}>
+          <h2>Search Transcript:</h2>
+          <div>
+            <input type="text" value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+          <div>
+            {foundWords.length > 0 && (
+              <div>
+                <h3>Found Words:</h3>
+                <div style={{backgroundColor: "white", color: "black", padding: "20px"}}>
+                {foundWords.map((foundWord, index) => (
+                  <p className="search" key={index} onClick={() => scrollToLine(foundWord.timestamp)} style={{cursor:'pointer'}}>
+                    {foundWord.timestamp}: {foundWord.word}
+                  </p>
+                ))}
+                </div>
+              </div>
+            )}
+            {foundWords.length === 0 && (
+              <p style={{backgroundColor: "white", color: "black", padding: "20px"}}>No matching words found.</p>
+            )}
+          </div>
         </div>
       </div>
     </>
